ce1a50f0050dad0b7b39f64a8293c938145f31c2
Alex <stingermssl@gmail.com>
2019-08-13 18:43:07 +0200

    Upgrade patch version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Hex.Mixfile do
 
   def project do
     [app: :hexgrid,
-     version: "2.1.0",
+     version: "2.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
